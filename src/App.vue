842be868e588527847ce9ea44a925b76f09e6a57
zhaonn <zhaonn@knowbox.cn>
2022-08-01 15:01:53 +0800

    引入element

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <left-list></left-list>
-  <right-content></right-content>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import LeftList from "@/components/LeftList";
-import RightContent from "@/components/RightContent";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    RightContent,
-    LeftList,
+    HelloWorld
   }
 }
 </script>
 
 <style>
-body {
-  margin: 0;
-}
 #app {
-  position: relative;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
