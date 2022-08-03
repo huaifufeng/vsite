e76d30d7d19e45c41192fc2ac97393a1a21e8625
zhaonn <zhaonn@knowbox.cn>
2022-08-03 17:45:15 +0800

    更新ui为plus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
