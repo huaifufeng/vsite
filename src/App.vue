51a63ce0bf36f9a81a19b2d5ab45ed3325a644be
zhaonn <zhaonn@knowbox.cn>
2022-08-03 18:34:20 +0800

    设置格局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div></div>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px" class="left-title">
+        <ul>
+          <li>羽毛球</li>
+        </ul>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'app',
-  components: {
-  }
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+}
+
+.left-title li{
+  list-style-type: none;
 }
 </style>
